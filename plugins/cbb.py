0773d2fc38eadcefb300efcc398f31fdcf17b82a
Geli Geli <bestmunum@gmail.com>
2024-09-27 20:11:52 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text = (
-                f"<b>○ Creator : </b>Bot Father\n\n"
+                f"<b>○ Creator : </b>Bot Father\n"
                 f"<b>○ My best Friend : </b><a href='tg://user?id={user.id}'>{user.first_name}</a>\n\n"
                 f"○ I may be a bot, but I can still be your best digital friend!\n\n"
                 f"<b>○ Fun Fact : </b>If you click 'Close', I will disappear like a magician's trick! 🎩✨\n"
